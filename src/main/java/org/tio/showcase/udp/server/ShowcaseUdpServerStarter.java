20dc6e63091f2cc9efa3ba8235b137882bf5b6b0
talent-tan <tywo45@163.com>
2019-06-25 14:24:07 +0800

    init

---
+++ src/main/java/org/tio/showcase/udp/server/ShowcaseUdpServerStarter.java
@@ -0,0 +1,23 @@
+package org.tio.showcase.udp.server;
+
+import java.net.SocketException;
+
+import org.tio.core.udp.UdpServer;
+import org.tio.core.udp.UdpServerConf;
+
+/**
+ * @author tanyaowu
+ *
+ */
+public class ShowcaseUdpServerStarter {
+	/**
+	 * @param args
+	 * @throws SocketException 
+	 */
+	public static void main(String[] args) throws SocketException {
+		ShowcaseUdpHandler fpmsUdpHandler = new ShowcaseUdpHandler();
+		UdpServerConf udpServerConf = new UdpServerConf(3000, fpmsUdpHandler, 5000);
+		UdpServer udpServer = new UdpServer(udpServerConf);
+		udpServer.start();
+	}
+}
